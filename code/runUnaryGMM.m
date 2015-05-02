3853bab18f0dcb755b47f396e8731e5ea4462190
Zouzou555 <zouchuhang@gmail.com>
2015-05-02 16:03:31 -0500

    revised GMM
    learn GMM of foreground inside the cropped edge box and GMM of
    background from the other part of the whole image

--- code/runUnaryGMM.m
+++ code/runUnaryGMM.m
@@ -1,14 +1,14 @@
 % runThis for GMM
 
 % params
-%K = 5;
-%compactness = 0.5;
-addpath(genpath('./third_party/GMM/GMM-HMRF_v1/GMM-HMRF_v1'));
+K = 5;
+compactness = 0.5;
+addpath(genpath('/Users/zstufzxy/Desktop/Course/CS543/project/GMM-HMRF_v1'));
 
-k=5; % k: number of regions
+k=2; % k: number of regions
 g=3; % g: number of GMM components
 beta=1; % beta: unitary vs. pairwise
-EM_iter=10; % max num of iterations
+EM_iter=1; % max num of iterations
 MAP_iter=10; % max num of iterations
 
 
@@ -16,25 +16,32 @@ MAP_iter=10; % max num of iterations
 
 im = imread('apple_1_1_1_crop.png');
 depth = imread('apple_1_1_1_depthcrop.png');
+im_large = imread('apple_1_1_1.png');
+loc = load('apple_1_1_1_loc.txt'); 
 
 % run slic
 %[cIndMap, time, imgVis] = slic(im, K, compactness);
 
 % pixel-wise color GMM
+%mex BoundMirrorExpand.cpp;
+%mex BoundMirrorShrink.cpp;
 
 Y=double(im);
 Y(:,:,1)=gaussianBlur(Y(:,:,1),3);
 Y(:,:,2)=gaussianBlur(Y(:,:,2),3);
 Y(:,:,3)=gaussianBlur(Y(:,:,3),3);
 
+Y_large=double(im_large);
+Y_large(:,:,1)=gaussianBlur(Y_large(:,:,1),3);
+Y_large(:,:,2)=gaussianBlur(Y_large(:,:,2),3);
+Y_large(:,:,3)=gaussianBlur(Y_large(:,:,3),3);
+
 tic;
-fprintf('Performing k-means segmentation\n');
-[X, GMM]=image_kmeans(Y,k,g);
+fprintf('Performing FG/BG segmentation\n');
+%[X GMM]=image_kmeans(Y,k,g);
+[X, GMM]=FG_estimate(Y,Y_large,g, loc);
 imwrite(uint8(X*80),'initial labels.png');
 
-[X, GMM]=HMRF_EM(X,Y,GMM,k,g,EM_iter,MAP_iter,beta);
+[X, GMM]=HMRF_EM(X,Y_large,GMM,k,g,EM_iter,MAP_iter,beta);
 imwrite(uint8(X*80),'final labels.png');
 toc;
-
-% pixel-wise 3D GMM
-
